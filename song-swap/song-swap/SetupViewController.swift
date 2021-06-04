a34940ff156f6445cd8f0e23ff218bd4427f6ce3
Sravya Balasa <sravya.balasa@gmail.com>
2021-06-04 09:38:04 -0700

    save a profile picture to parse

--- song-swap/song-swap/SetupViewController.swift
+++ song-swap/song-swap/SetupViewController.swift
@@ -6,19 +6,58 @@
 //
 
 import UIKit
+import Parse
 
-class SetupViewController: UIViewController {
+class SetupViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var profilePicture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        usernameLabel.text =  "@" + (PFUser.current()?.username)!
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onSaveSetup(_ sender: Any) {
-        self.performSegue(withIdentifier: "setupToHome", sender: nil)
+        let currentUser = PFUser.current()!
+
+        let imageData = profilePicture.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        currentUser["profilePicture"] = file
+        
+        currentUser.saveInBackground { success, error in
+            if success {
+                self.performSegue(withIdentifier: "setupToHome", sender: nil)
+            } else {
+                print(error!)
+            }
+        }
     }
     
+    @IBAction func onAddProfilePicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
+        profilePicture.image = scaledImage
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
