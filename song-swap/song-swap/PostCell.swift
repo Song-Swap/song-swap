bceb2bee48e29129af3997586aff208c8546165a
farnaf <fnafarif@ucsd.edu>
2021-06-03 23:01:05 -0700

    updated home feed

---
+++ song-swap/song-swap/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  song-swap
+//
+//  Created by Farnia Nafarifard on 6/3/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
