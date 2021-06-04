156085554a6f7cd6ef08f86e62fc0f2e50b0d392
Sravya Balasa <sravya.balasa@gmail.com>
2021-06-04 11:33:53 -0700

    finished most of profile

---
+++ song-swap/song-swap/ProfilePostCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfilePostCell.swift
+//  song-swap
+//
+//  Created by Sravya Balasa on 6/4/21.
+//
+
+import UIKit
+
+class ProfilePostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
